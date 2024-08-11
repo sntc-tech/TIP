892a6d8c542958cdb0e5c000f076e539e051f9fe
sntc-tech <shubhamspadhi@gmail.com>
2024-08-11 19:02:27 +0530

    Added phone numbers

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,81 +4,86 @@ import Alert from "@/components/alert/alert";
 import ContactCard from "@/components/contact/contact-card";
 
 const ContactPage = () => {
-  return (
-    <div className="fade-in-page">
-      <ContactHeader />
-      <div className="flex flex-col items-center px-5 py-12 pb-24 md:px-12">
-        <Alert variant="warning">
-          We prefer mails/texts over calls, so that we can reply at our own
-          convenience.
-        </Alert>
-      </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5 px-12 pb-24">
-        <div className="md:col-span-2 xl:col-span-3 text-4xl md:text-6xl font-semibold tracking-tight text-center md:text-left">
-          SnTC Team
-        </div>
-        <ContactCard
-          name={"Vaibhav Kesharwani"}
-          phone={9369080567}
-          mail={"technical_secretary@students.iitmandi.ac.in"}
-        />
-        <ContactCard
-          name={"Aryan Singh"}
-          phone={6393051440}
-          mail={"b22260@students.iitmandi.ac.in"}
-        />
-        <ContactCard
-          name={"Aritra Boral"}
-          phone={9433823273}
-          mail={"b22198@students.iitmandi.ac.in"}
-        />
-      </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5 px-12 pb-24">
-        <div className="md:col-span-2 xl:col-span-3 text-4xl md:text-6xl font-semibold tracking-tight text-center md:text-left">
-          Technical Society Clubs
-        </div>
-        <ContactCard name={"KamandPrompt"} mail={"pc@iitmandi.ac.in"} />
-        <ContactCard
-          name={"Yantrik Club"}
-          mail={"yantrik_club@students.iitmandi.ac.in"}
-        />
-        <ContactCard
-          name={"Robotronics Club"}
-          mail={"robotronics@students.iitmandi.ac.in"}
-        />
-        <ContactCard name={"E-Cell"} mail={"edc@students.iitmandi.ac.in"} />
-        <ContactCard
-          name={"Space Technology and Astronomy Club"}
-          mail={"stac@students.iitmandi.ac.in"}
-        />
-        <ContactCard
-          name={"Society of Automotive Engineers"}
-          mail={"supra@students.iitmandi.ac.in"}
-        />
-        <ContactCard
-          name={"Nirmaan Club"}
-          mail={"nirmaan_club@students.iitmandi.ac.in"}
-        />
-        <ContactCard
-          name={"System Administration and Infosec Cell"}
-          mail={"saic@students.iitmandi.ac.in"}
-        />
-      </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5 px-12 pb-24">
-        <div className="md:col-span-2 xl:col-span-3 text-4xl md:text-6xl font-semibold tracking-tight text-center md:text-left">
-          TIP Website Team
-        </div>
-        <ContactCard
-          name={"Shubham S Padhi"}
-          mail={"b23295@students.iitmandi.ac.in"}
-        />
-        <ContactCard
-          name={"Vinamra Garg"}
-          mail={"b23302@students.iitmandi.ac.in"}
-        />
-      </div>
-    </div>
-  );
+	return (
+		<div className="fade-in-page">
+			<ContactHeader/>
+			<div className="flex flex-col items-center px-5 py-12 pb-24 md:px-12">
+				<Alert variant="warning">
+					We prefer mails/texts over calls, so that we can reply at our own
+					convenience.
+				</Alert>
+			</div>
+			<div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5 px-12 pb-24">
+				<div
+					className="md:col-span-2 xl:col-span-3 text-4xl md:text-6xl font-semibold tracking-tight text-center md:text-left">
+					SnTC Team
+				</div>
+				<ContactCard
+					name={"Vaibhav Kesharwani"}
+					phone={9369080567}
+					mail={"technical_secretary@students.iitmandi.ac.in"}
+				/>
+				<ContactCard
+					name={"Aryan Singh"}
+					phone={6393051440}
+					mail={"b22260@students.iitmandi.ac.in"}
+				/>
+				<ContactCard
+					name={"Aritra Boral"}
+					phone={9433823273}
+					mail={"b22198@students.iitmandi.ac.in"}
+				/>
+			</div>
+			<div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5 px-12 pb-24">
+				<div
+					className="md:col-span-2 xl:col-span-3 text-4xl md:text-6xl font-semibold tracking-tight text-center md:text-left">
+					TIP Website Team
+				</div>
+				<ContactCard
+					name={"Shubham S Padhi"}
+					mail={"b23295@students.iitmandi.ac.in"}
+					phone={9043674629}
+				/>
+				<ContactCard
+					name={"Vinamra Garg"}
+					mail={"b23302@students.iitmandi.ac.in"}
+					phone={9350555053}
+				/>
+			</div>
+			<div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5 px-12 pb-24">
+				<div
+					className="md:col-span-2 xl:col-span-3 text-4xl md:text-6xl font-semibold tracking-tight text-center md:text-left">
+					Technical Society Clubs
+				</div>
+				<ContactCard name={"KamandPrompt"} mail={"pc@iitmandi.ac.in"}/>
+				<ContactCard
+					name={"Yantrik Club"}
+					mail={"yantrik_club@students.iitmandi.ac.in"}
+				/>
+				<ContactCard
+					name={"Robotronics Club"}
+					mail={"robotronics@students.iitmandi.ac.in"}
+				/>
+				<ContactCard name={"E-Cell"} mail={"edc@students.iitmandi.ac.in"}/>
+				<ContactCard
+					name={"Space Technology and Astronomy Club"}
+					mail={"stac@students.iitmandi.ac.in"}
+				/>
+				<ContactCard
+					name={"Society of Automotive Engineers"}
+					mail={"supra@students.iitmandi.ac.in"}
+				/>
+				<ContactCard
+					name={"Nirmaan Club"}
+					mail={"nirmaan_club@students.iitmandi.ac.in"}
+				/>
+				<ContactCard
+					name={"System Administration and Infosec Cell"}
+					mail={"saic@students.iitmandi.ac.in"}
+				/>
+			</div>
+		</div>
+	);
 };
 
 export default ContactPage;
--- firebase/firebase.ts
+++ firebase/firebase.ts
@@ -1,22 +1,22 @@
-import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import {initializeApp} from "firebase/app";
+import {getAuth, GoogleAuthProvider, signInWithPopup} from "firebase/auth";
+import {getFirestore} from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAuMJD0yKrtBVmd1fddPmI0NkDvuQeDq-k",
-  authDomain: "test-project-e804f.firebaseapp.com",
-  projectId: "test-project-e804f",
-  storageBucket: "test-project-e804f.appspot.com",
-  messagingSenderId: "978336391253",
-  appId: "1:978336391253:web:6b036311742326ec63b4dc",
+	apiKey: "AIzaSyAuMJD0yKrtBVmd1fddPmI0NkDvuQeDq-k",
+	authDomain: "test-project-e804f.firebaseapp.com",
+	projectId: "test-project-e804f",
+	storageBucket: "test-project-e804f.appspot.com",
+	messagingSenderId: "978336391253",
+	appId: "1:978336391253:web:6b036311742326ec63b4dc",
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
 export const db = getFirestore(firebaseApp);
 const googleProvider = new GoogleAuthProvider();
-googleProvider.setCustomParameters({ prompt: "select_account" });
+googleProvider.setCustomParameters({prompt: "select_account"});
 
 const auth = getAuth();
 
 export const signInWithGooglePopup = () =>
-  signInWithPopup(auth, googleProvider);
+	signInWithPopup(auth, googleProvider);
