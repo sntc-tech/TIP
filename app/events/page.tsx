c4cc9ce57bfc767e722ec4d0aa6c52d2e3b61ada
sntc-tech <shubhamspadhi@gmail.com>
2024-08-13 14:36:49 +0530

    Added gallery feature

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,77 +1,77 @@
 "use client";
 
-import React, { useContext, useEffect, useState } from "react";
+import React, {useContext, useEffect, useState} from "react";
 import EventsHeader from "@/components/events/events-header";
 import JumpBar from "@/components/events/jump-bar";
 import Alert from "@/components/alert/alert";
-import { UserContext } from "@/context/user-context";
-import { getUserDoc } from "@/lib/actions";
-import { doc, updateDoc } from "firebase/firestore";
-import { db } from "@/firebase/firebase";
-import { eventData } from "@/lib/event-data";
+import {UserContext} from "@/context/user-context";
+import {getUserDoc} from "@/lib/actions";
+import {doc, updateDoc} from "firebase/firestore";
+import {db} from "@/firebase/firebase";
+import {eventData} from "@/lib/event-data";
 import EventSection from "@/components/events/event-section";
-import { useRouter } from "next/navigation";
+import {useRouter} from "next/navigation";
 
 const EventsPage = () => {
-  const [regEvents, setRegEvents] = useState<string[]>();
-  const { currentUserID, setCurrentUserID } = useContext(UserContext);
-  const router = useRouter();
-
-  useEffect(() => {
-    (async () => {
-      if (currentUserID) {
-        try {
-          const { registeredEvents } = await getUserDoc(currentUserID);
-          setRegEvents(registeredEvents);
-        } catch (error) {
-          setCurrentUserID(null);
-          await router.push("/");
-        }
-      }
-    })();
-  }, []);
-
-  useEffect(() => {
-    (async () => {
-      if (currentUserID) {
-        const userDoc = doc(db, "users", currentUserID);
-        if (regEvents) {
-          try {
-            await updateDoc(userDoc, {
-              registeredEvents: regEvents,
-            });
-          } catch (error) {
-            setCurrentUserID(null);
-            await router.push("/");
-          }
-        }
-      }
-    })();
-  }, [regEvents]);
-
-  return (
-    <div className="relative fade-in-page">
-      <EventsHeader />
-      <div className="sticky top-16 sm:top-20 z-40">
-        <JumpBar />
-      </div>
-      <div></div>
-      <div className="flex flex-col items-center gap-5 px-5 py-12 md:px-12">
-        <Alert variant="warning">
-          Since the venue can only accommodate a maximum of 350 students, only
-          the first 350 students to register will be considered.
-        </Alert>
-        {eventData.map((event) => (
-          <EventSection
-            props={event}
-            key={event.id}
-            regEvents={regEvents}
-            setRegEvents={setRegEvents}
-          />
-        ))}
-      </div>
-    </div>
-  );
+	const [regEvents, setRegEvents] = useState<string[]>();
+	const {currentUserID, setCurrentUserID} = useContext(UserContext);
+	const router = useRouter();
+	
+	useEffect(() => {
+		(async () => {
+			if (currentUserID) {
+				try {
+					const {registeredEvents} = await getUserDoc(currentUserID);
+					setRegEvents(registeredEvents);
+				} catch (error) {
+					setCurrentUserID(null);
+					await router.push("/");
+				}
+			}
+		})();
+	}, []);
+	
+	useEffect(() => {
+		(async () => {
+			if (currentUserID) {
+				const userDoc = doc(db, "users", currentUserID);
+				if (regEvents) {
+					try {
+						await updateDoc(userDoc, {
+							registeredEvents: regEvents,
+						});
+					} catch (error) {
+						setCurrentUserID(null);
+						await router.push("/");
+					}
+				}
+			}
+		})();
+	}, [regEvents]);
+	
+	return (
+		<div className="relative fade-in-page">
+			<EventsHeader/>
+			<div className="sticky top-16 sm:top-20 z-40">
+				<JumpBar/>
+			</div>
+			<div></div>
+			<div className="flex flex-col items-center gap-5 px-5 py-12 md:px-12">
+				<Alert variant="warning">
+					Since the venue can only accommodate a maximum of 350 students, only
+					the first 350 students to register will be considered.
+				</Alert>
+				{eventData.map((event) => (
+					<EventSection
+						props={event}
+						key={event.id}
+						regEvents={regEvents}
+						setRegEvents={setRegEvents}
+					/>
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default EventsPage;
--- lib/event-data.ts
+++ lib/event-data.ts
@@ -8,7 +8,9 @@ export const eventData = [
 		location: "A18-1A",
 		desc: "Call those logical brain cells of yours to work in the programming fundamentals session. Have a sneak peak at what’s in for you: We'll explore the basics: the concept and goals of programming, variables (like data containers), different data types (think numbers and words), and control structures (the rules that guide programs). We'll dive into Python (friendly and versatile) and C++ (powerful and fast) to explore their syntax, and compare the two languages on various grounds, including their use cases. Plus, we'll set up Arduino IDE for hands-on learning in the upcoming sessions. Don't worry if it's new—programming is all about learning by doing! Let's make coding fun and accessible together.",
 		by: "KamandPrompt and the Robotronics Club",
-		contact: "pc@iitmandi.ac.in",
+		contact: "pc@students.iitmandi.ac.in",
+		completed: true,
+		gallery: "https://drive.google.com/drive/folders/1AmQkwf9zd-fPMCw3CjRZpqdASdL5En_D",
 	},
 	{
 		id: "neKqtIjqUhc0QeHrpxek",
@@ -20,6 +22,8 @@ export const eventData = [
 		desc: "Join us for an insightful session on microcontrollers and sensors, part of the Technical Induction Program organized by the SnTC core team. This lecture will introduce you to the basics of microcontroller architecture, sensor interfacing, and essential programming techniques. You'll gain practical knowledge and hands-on experience that will be invaluable for your projects and coursework. Whether you're new to the world of microcontrollers or looking to deepen your understanding, this session is designed to equip you with the foundational skills needed for your first year and beyond. Register now to kickstart your technical journey!",
 		by: "Yantrik and the Robotronics Club",
 		contact: "robotronics@students.iitmandi.ac.in",
+		completed: false,
+		gallery: "",
 	},
 	{
 		id: "DYT3hZXSs38QGrCqEno9",
@@ -31,6 +35,8 @@ export const eventData = [
 		desc: "Join us for an insightful session dedicated to enhancing your academic and research journey at IIT Mandi. This event, hosted by the Tech Secretary, will cover essential topics to help you build a strong profile for research positions during your undergraduate studies. Learn how to effectively interact with faculty members to maximize the benefits of these relationships. Discover the diverse and cutting-edge labs at IIT Mandi and explore the hidden gems within our institute. This session will also provide guidance on crafting a robust profile to ensure future academic and research success.\n\nDon't miss this opportunity to gain valuable insights and practical advice to navigate your academic journey with confidence and excellence.",
 		by: "the Tech Secretary",
 		contact: "technical_secretary@students.iitmandi.ac.in",
+		completed: false,
+		gallery: "",
 	},
 	{
 		id: "X5MACE2nBwTgLQWp5p0k",
@@ -42,6 +48,8 @@ export const eventData = [
 		desc: "Embark on an exciting journey into the world of SolidWorks and 3D printing as part of the Technical Induction Program organized by the SnTC core team. This lecture will introduce you to the fundamentals of SolidWorks, featuring practical examples and interactive Q&A sessions to deepen your understanding. You will also explore the significant applications of 3D printing, demonstrating how it transforms the design and manufacturing process by making it more efficient, innovative, and cost-effective. Whether you are new to SolidWorks and 3D printing or looking to broaden your knowledge, this session is designed to equip you with essential skills for your first year and beyond. Register now to start your technical journey with us!",
 		by: "Yantrik Club",
 		contact: "yantrik_club@students.iitmandi.ac.in",
+		completed: false,
+		gallery: "",
 	},
 	{
 		id: "OL3mbDsLfSWGT1pkEATm",
@@ -53,6 +61,8 @@ export const eventData = [
 		desc: "Discover the essentials of the stock market and startup development in this engaging session. Organized by the E-Cell team, this lecture will introduce key concepts of market dynamics, investment strategies, and financial literacy. You'll also explore the stages of startup development, from ideation and scaling to exit strategies. Whether you're an aspiring entrepreneur or keen on understanding market trends, this session provides essential insights and practical knowledge. Don’t miss this chance to enhance your financial and entrepreneurial skills. Register now to embark on your journey into the world of stocks and startups!",
 		by: "E-Cell",
 		contact: "edc@students.iitmandi.ac.in",
+		completed: false,
+		gallery: "",
 	},
 	{
 		id: "Y9IHnTEHTVhYNDvh7Gpu",
@@ -63,7 +73,9 @@ export const eventData = [
 		location: "A18-1A",
 		desc: "As part of our Technical Induction Programme (TIP), SnTC is excited to announce a session on 'Intro to Git/GitHub and Project Management' for first-year students on 23-Aug-2024. This session will provide a comprehensive overview of version control, teaching students how to create repositories and manage branches effectively. Additionally, participants will learn how to utilize Git commands within Visual Studio Code to streamline their project workflows. This foundational knowledge will equip students with essential skills for collaborative development and efficient project management, setting the stage for their future technical endeavors.",
 		by: "KamandPrompt",
-		contact: "pc@iitmandi.ac.in",
+		contact: "pc@students.iitmandi.ac.in",
+		completed: false,
+		gallery: "",
 	},
 	{
 		id: "EUizKAGgkUoCXE7mOLHY",
@@ -74,7 +86,9 @@ export const eventData = [
 		location: "A18-2A",
 		desc: "As a part of the Technical Induction Program of the SnTC Society, We are thrilled to provide a  session aimed at revolutionizing your technical proficiency and augmenting your comprehension of essential technologies like Linux, dualboot and DevOps:\nDiving into these topics, you will get to explore the foundation of many modern computing environments. Linux, known for its open-source nature, stability, and security, is a vital operating system in the tech industry. Understanding dual booting will empower you to install and use Linux alongside another OS on your computer, giving you the flexibility to leverage the strengths of both systems. Finally, DevOps practices, which integrate software development and IT operations, will equip you with the skills to automate processes, enhance collaboration, and streamline workflows, crucial for any aspiring developer or system administrator. This session will provide you with valuable knowledge and practical skills that are highly sought after in today's tech landscape.",
 		by: "CG2D and KamandPrompt",
-		contact: "pc@iitmandi.ac.in",
+		contact: "pc@students.iitmandi.ac.in",
+		completed: false,
+		gallery: "",
 	},
 	{
 		id: "a0apMDoK6py5VCOWjufx",
@@ -86,5 +100,7 @@ export const eventData = [
 		desc: "As part of our Technical Induction Programme (TIP) organized by SnTC, we are thrilled to present a session on 'Cybersecurity, Blockchain, and Generative AI' for first-year students on 30-Aug-2024. This session aims to provide an understanding of the fundamentals of cybersecurity and its critical importance in the digital age. Students will also gain a basic understanding of blockchain technology and its underlying principles. Additionally, the session will introduce the concept of Generative AI, exploring how it differs from other AI technologies. This session will equip students with essential knowledge of these cutting-edge fields, fostering a deeper interest and readiness for future innovations.",
 		by: "SAIC",
 		contact: "saic@students.iitmandi.ac.in",
+		completed: false,
+		gallery: "",
 	},
 ];
